e99704171f95957dd928d74886ac998930e30fb3
vojtob <vojtech.balint@dxc.com>
2018-05-27 22:16:12 +0200

    test deploy pre API

---
+++ temp/runAPI/dataAccess.js
@@ -0,0 +1,90 @@
+var AWS = require("aws-sdk");
+AWS.config.update({
+
+    region: "eu-central-1",
+    endpoint: "http://localhost:8000"
+});
+// AWS.config.update({endpoint: "https://dynamodb.eu-central-1.amazonaws.com"});
+
+var docClient = new AWS.DynamoDB.DocumentClient()
+
+// function getTeamCount(teamID) {
+//     return teams.length;
+// }
+
+function getTeam(teamID, callback) {
+    getItem("TheRunTeam", { team: teamID }, callback);
+}
+
+function getLeg(legIndex, callback) {
+    getItem("TheRunRoute", { legID: legIndex }, callback);
+}
+
+function getLegs(callback) {
+    var params = {
+        TableName: "TheRunRoute",
+    };
+
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            console.error("Unable to scan items. Error JSON:", JSON.stringify(err, null, 2));
+            callback(err, null);
+        } else {
+            console.log("Scan succeeded");
+            // console.log("Scan succeeded:", JSON.stringify(data, null, 2));
+            callback(null, data.Items);
+        }
+    });
+}
+
+function getItem(tableName, keyParam, callback) {
+    console.log("get" + tableName + " " + JSON.stringify(keyParam));
+    var params = {
+        TableName: tableName,
+        Key: keyParam
+    };
+    console.log(params);
+
+    docClient.get(params, function (err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            callback(err, null);
+        } else {
+            // console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            callback(null, data.Item);
+        }
+    });
+}
+
+function putTeam(team, callback) {
+    var params = {
+        TableName: "TheRunTeam",
+        Item: team
+    }
+
+    generalPut(params, callback);
+    // docClient.put(params, function (err, data) {
+    //     if (err) {
+    //         console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    //         callback(err, data);
+    //     } else {
+    //         console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    //         callback(err, data);
+    //     }
+    // });
+}
+
+function generalPut(params, callback) {
+    docClient.put(params, function (err, data) {
+        callback(err, data);
+    });
+}
+
+exports.getTeam = getTeam;
+exports.getLeg = getLeg;
+exports.getLegs = getLegs;
+exports.putTeam = putTeam;
+
+exports.generalPut = generalPut;
+
+// exports.getTeamCount = getTeamCount;
---
+++ temp/runAPI/index.js
@@ -0,0 +1,194 @@
+var info = require("./info");
+var results = require("./results");
+
+exports.handler = (event, context, callback) => {
+    console.log('Received event: ' + JSON.stringify(event));
+
+    if ((event.httpMethod == "GET") && (event.resource == "/legs")) {
+        // get legs
+        getLegs(event, context, callback);
+    } else if ((event.httpMethod == "GET") && (event.resource == "/teams")) {
+        // get teams
+        getTeams(event, context, callback);
+    } else if ((event.httpMethod == "GET") && (event.resource == "/results")) {
+        // get results
+        getResults(event, context, callback);
+    } else if ((event.httpMethod == "POST") && (event.resource == "/results")) {
+        // get results
+        postResults(event, context, callback);
+    } else {
+        var problem = { message: "nezname volanie" };
+        sendError(problem, callback);
+    }
+};
+
+function getLegs(event, context, callback) {
+    if (event.queryStringParameters === null || event.queryStringParameters === undefined) {
+        // bez parametrov
+        sendLegs(callback);
+        return;
+    }
+
+    if (checkParam(event.queryStringParameters, "legID")) {
+        var legID = parseInt(event.queryStringParameters.legID);
+        console.log("GET results for legID: " + legID);
+        info.getLeg(legID, function (err, leg) {
+            if (err) {
+                sendError(err, callback);
+            } else {
+                sendLeg(leg, callback);
+            }
+        });
+    } else {
+        sendLegs(callback);
+    }
+}
+
+function getTeams(event, context, callback) {
+}
+
+function getResults(event, context, callback) {
+    console.log("process results");
+    if (event.queryStringParameters === null || event.queryStringParameters === undefined) {
+        // bez parametrov
+        sendError({ message: "neviem ake vysledky chces" }, callback);
+        return;
+    }
+    if (!checkParam(event.queryStringParameters, "teamID")) {
+        sendError({ message: "musi byt definovane teamID" }, callback);
+        return;
+    }
+    var teamID = parseInt(event.queryStringParameters.teamID);
+    console.log("Request results for team: " + teamID);
+
+    if (checkParam(event.queryStringParameters, "legID")) {
+        // vysledky teamu na useku
+        var legID = parseInt(event.queryStringParameters.legID);
+        console.log("Request result for legID: " + legID);
+        results.getResult(teamID, legID, function (err, result) {
+            if (err) {
+                sendError(err, callback);
+                return;
+            } else {
+                sendOK(result, callback);
+                return;
+            }
+        });
+    } else {
+        // kompletne vysledky teamu
+        console.log("Request team results");
+        results.getTeamResult(teamID, function (err, result) {
+            if (err) {
+                sendError(err, callback);
+                return;
+            } else {
+                sendOK(result, callback);
+                return;
+            }
+        });
+    }
+}
+
+function postResults(event, context, callback) {
+    console.log("post results processing ...");
+    if (event.queryStringParameters === null || event.queryStringParameters === undefined) {
+        // bez parametrov
+        console.log("neda sa zaslat vysledok bez parametrov")
+        sendError({ message: "neda sa zaslat vysledok bez parametrov" }, callback);
+        return;
+    }
+    if (!checkParam(event.queryStringParameters, "teamID")) {
+        console.log("pre zadanie vysledku musi byt definovany team")
+        sendError({ message: "pre zadanie vysledku musi byt definovany team" }, callback);
+        return;
+    }
+    var teamID = parseInt(event.queryStringParameters.teamID);
+    console.log("Post results for team: " + teamID);
+
+    if (!checkParam(event.queryStringParameters, "legID")) {
+        console.log("pre zadanie vysledku musi byt definovany leg")
+        sendError({ message: "pre zadanie vysledku musi byt definovany leg" }, callback);
+        return;
+    }
+    var legID = parseInt(event.queryStringParameters.legID);
+    console.log("Post results for leg: " + legID);
+
+    if (checkParam(event.queryStringParameters, "legDuration")) {
+        var legDuration = event.queryStringParameters.legDuration;
+        console.log("set leg " + legID + " to " + legDuration + " for team " + teamID);
+        results.setRealDuration(teamID, legID, legDuration, function (err, result) {
+            if (err) {
+                console.log("could not set leg duration");
+                console.log(JSON.stringify(err));
+                sendError(err, callback);
+                return;
+            } else {
+                console.log("leg duration set successfully");
+                // console.log(JSON.stringify(result));
+                sendOK("OK", callback);
+                return;
+            }
+        });
+    } else {
+        console.log("clear leg " + legID + " for team " + teamID);
+        results.clearRealDuration(teamID, legID, function (err, result) {
+            if (err) {
+                console.log("could not clear leg duration");
+                console.log(JSON.stringify(err));
+                sendError(err, callback);
+                return;
+            } else {
+                console.log("leg duration cleared successfully");
+                // console.log(JSON.stringify(result));
+                sendOK("OK", callback);
+                return;
+            }
+        });
+    }
+}
+
+
+function sendLeg(leg, callback) {
+    sendOK(leg, callback);
+}
+
+function sendLegs(callback) {
+    console.log("no specific legID -> info about all legs");
+    var legs = [];
+    for (var i = 1; i < 49; i++) {
+        legs.push(i);
+    }
+    sendOK(legs, callback);
+}
+
+function sendError(err, callback) {
+    var response = {
+        statusCode: 400,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(err)
+    };
+    callback(err, response);
+}
+
+function sendOK(data, callback) {
+    var response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(data)
+    };
+    // console.log(response);
+    callback(null, response);
+}
+
+function checkParam(queryStringParameters, paramName) {
+    if (!queryStringParameters.hasOwnProperty(paramName)) {
+        return false;
+    }
+    return (queryStringParameters[paramName] !== null &&
+        queryStringParameters[paramName] !== "");
+}
+
